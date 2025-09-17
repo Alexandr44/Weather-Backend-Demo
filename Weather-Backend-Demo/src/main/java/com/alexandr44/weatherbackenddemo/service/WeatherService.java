da212c94df3288d229f37c241c51148e8edcb6e7
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-09-17 14:56:58 +0500

    Added cache to requests

--- Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/service/WeatherService.java
+++ Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/service/WeatherService.java
@@ -1,14 +1,18 @@
 package com.alexandr44.weatherbackenddemo.service;
 
 import com.alexandr44.weatherbackenddemo.client.WeatherClient;
+import com.alexandr44.weatherbackenddemo.config.CacheConfig;
 import com.alexandr44.weatherbackenddemo.dto.OpenWeatherResponse;
 import com.alexandr44.weatherbackenddemo.dto.WeatherDto;
 import com.alexandr44.weatherbackenddemo.mapper.WeatherMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WeatherService {
@@ -25,14 +29,18 @@ public class WeatherService {
     private final WeatherClient weatherClient;
     private final WeatherMapper weatherMapper;
 
+    @Cacheable(value = CacheConfig.WEATHER_CACHE_NAME, key = "#city + #countryCode")
     public WeatherDto getWeatherByCity(final String city, final String countryCode) {
+        log.debug("Requesting weather by city with params: city {}, countryCode {}", city, countryCode);
         final ResponseEntity<OpenWeatherResponse> response = weatherClient.getWeatherByCity(
             buildQuery(city, countryCode), apiKey, units, language
         );
         return weatherMapper.map(response.getBody());
     }
 
+    @Cacheable(value = CacheConfig.WEATHER_CACHE_NAME, key = "#lon + #lat")
     public WeatherDto getWeatherByCoordinates(final Double lon, final Double lat) {
+        log.debug("Requesting weather by coordinates with params: lon {}, lat {}", lon, lat);
         final ResponseEntity<OpenWeatherResponse> response = weatherClient.getWeatherByCoordinates(
             lon, lat, apiKey, units, language
         );
