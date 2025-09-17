a4609afeb871308843e6b1ac879ac4b55100bc81
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-09-17 15:59:25 +0500

    Added history controller and logic

--- Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/repository/HistoryRepository.java
+++ Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/repository/HistoryRepository.java
@@ -1,10 +1,22 @@
 package com.alexandr44.weatherbackenddemo.repository;
 
 import com.alexandr44.weatherbackenddemo.entity.HistoryItem;
+import com.alexandr44.weatherbackenddemo.enums.HistoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistoryRepository extends JpaRepository<HistoryItem, Long> {
+
+    List<HistoryItem> findAllByCityAndCountryCodeAndLonAndLat(
+        final String city,
+        final String countryCode,
+        final Double lon,
+        final Double lat
+    );
+
+    int countAllByType(final HistoryType historyType);
 
 }
--- Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/service/HistoryService.java
+++ Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/service/HistoryService.java
@@ -1,11 +1,14 @@
 package com.alexandr44.weatherbackenddemo.service;
 
+import com.alexandr44.weatherbackenddemo.dto.HistoryResponse;
 import com.alexandr44.weatherbackenddemo.entity.HistoryItem;
 import com.alexandr44.weatherbackenddemo.enums.HistoryType;
 import com.alexandr44.weatherbackenddemo.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -33,8 +36,60 @@ public class HistoryService {
         );
     }
 
+    public HistoryResponse getHistoryStatistic(
+        final String city,
+        final String countryCode,
+        final Double lon,
+        final Double lat
+    ) {
+        final List<HistoryItem> historyItems = historyRepository.findAllByCityAndCountryCodeAndLonAndLat(
+            city, countryCode, lon, lat
+        );
+
+        return new HistoryResponse()
+            .setParams(buildParamsString(city, countryCode, lon, lat))
+            .setCount(historyItems.size());
+    }
+
+    public HistoryResponse getHistoryStatisticByType(final HistoryType type) {
+        return new HistoryResponse()
+            .setParams("type=" + type.name())
+            .setCount(historyRepository.countAllByType(type));
+    }
+
     private void saveHistoryItem(final HistoryItem historyItem) {
         historyRepository.save(historyItem);
+    }
+
+    private String buildParamsString(
+        final String city,
+        final String countryCode,
+        final Double lon,
+        final Double lat
+    ) {
+        final StringBuilder params = new StringBuilder();
+        if (city != null) {
+            params.append("city=").append(city);
+        }
+        if (countryCode != null) {
+            if (!params.isEmpty()) {
+                params.append("&");
+            }
+            params.append("countryCode=").append(countryCode);
+        }
+        if (lon != null) {
+            if (!params.isEmpty()) {
+                params.append("&");
+            }
+            params.append("lon=").append(lon);
+        }
+        if (lat != null) {
+            if (!params.isEmpty()) {
+                params.append("&");
+            }
+            params.append("lat=").append(lat);
+        }
+        return params.toString();
     }
 
 }
