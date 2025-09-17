2d0db1060dc2aaf8c5a71927a7ca0af2d82a73cb
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-09-17 11:15:28 +0500

    Init project

---
+++ Weather-Backend-Demo/src/main/java/com/alexandr44/weatherbackenddemo/WeatherBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alexandr44.weatherbackenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBackendDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherBackendDemoApplication.class, args);
+    }
+
+}
---
+++ Weather-Backend-Demo/src/test/java/com/alexandr44/weatherbackenddemo/WeatherBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandr44.weatherbackenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackendDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
